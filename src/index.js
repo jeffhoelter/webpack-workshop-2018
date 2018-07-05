610fc84f77e4c7ede3c107ca5b43e095b4ff2bff
Jeff Hoelter <jeffh@loar.net>
2018-07-05 15:45:53 -0400

    Simple webpack bundling examples.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import nav from "./nav";
+import { top, bottom } from "./footer";
+import makeButton from "./button";
+import { red } from "./button-styles";
 
-console.log(nav);
+console.log(nav(), top, bottom, makeButton("hello"), red);
