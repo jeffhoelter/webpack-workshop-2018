25ef84c54d0ee854477567e5aac389b007ab31c2
Jeff Hoelter <jeffh@loar.net>
2018-07-09 16:09:44 -0400

    Updated webpack-command dependency to fix a bug in it.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+// webpack v4
 const path = require("path");
-
 module.exports = {
-  mode: "development"
+  mode: "development",
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  }
 };
