8405d30baf7f069b788f43e00e8ee8774eb9cd80
Jeff Hoelter <jeffh@loar.net>
2018-07-10 09:39:58 -0400

    Added HtmlWebpackPlugin and webpack.ProgressPlugin to the webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 // webpack v4
 const path = require("path");
-module.exports = {
-  mode: "development",
-  entry: { main: "./src/index.js" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
-  }
+const webpack = require("webpack");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = ({ mode }) => {
+  return {
+    mode: "development",
+    entry: { main: "./src/index.js" },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js"
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+  };
 };
